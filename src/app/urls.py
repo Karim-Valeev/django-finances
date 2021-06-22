bdbc9d44fd8848873c6d54e80643da7b5c09b3ca
zanzydnd <zanzydnd@gmail.com>
2021-06-22 20:21:06 +0300

    sign_in and up pages

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
 
-from app.views import test_view
+from app.views import test_view, sign_up, sign_in
 
-urlpatterns=[
+urlpatterns = [
     path("", test_view),
+    path("sign_up/", sign_up, name="sign_up"),
+    path("sign_in/", sign_in, name="sign_in")
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def test_view(request):
     return render(request, "pages/main.html")
+
+
+def sign_up(request):
+    return render(request, "pages/sign_up.html")
+
+def sign_in(request):
+    return render(request, "pages/sign_in.html")
