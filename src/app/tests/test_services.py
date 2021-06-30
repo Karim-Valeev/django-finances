43444bc945c2c1dbf24b7b13b60d3c258712d0b9
Karim-Valeev <karim.valeev.i@gmail.com>
2021-06-30 16:55:29 +0300

    Add login view test

---
+++ src/app/tests/test_services.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AnonymousUser
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase, RequestFactory
+from django.test import Client
+# import mock
+from django.urls import reverse
