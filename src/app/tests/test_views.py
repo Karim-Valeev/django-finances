e43d384eec8087c5f99c942a8d1e0d329126ef6c
Danil Galeev <galoffdan@gmail.com>
2021-07-01 14:42:04 +0300

    write views tests

--- src/app/tests/test_views.py
+++ src/app/tests/test_views.py
@@ -1,11 +1,15 @@
+from datetime import datetime
 from django.contrib.auth.models import AnonymousUser
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, RequestFactory
 
 from app.models import WalletUser
 from app.models import Note
-from app.views import sign_in
+from app.models.note import NoteType
+from app.views import sign_in, delete_note_view
 # from barbook_app.views import CreateCocktail
+import pytest
+from pytest_django.asserts import assertTemplateUsed
 
 
 class FinanceViewTestCases(TestCase):
@@ -26,6 +30,7 @@ class FinanceViewTestCases(TestCase):
         # Todo проверить, что нам вернется main page
         response = sign_in(request)
         self.assertTemplateUsed(template_name='main.html')
+
     #
     # def test_create_cocktail_view(self):
     #     request = self.factory.get("")
@@ -34,4 +39,40 @@ class FinanceViewTestCases(TestCase):
     #     self.assertEqual(response.status_code, 200)
 
 
+@pytest.fixture
+def create_user(db):
+    def make_user():
+        return WalletUser.objects.create(
+            email='test@test.com', password='test_password'
+        )
+    return make_user
 
+
+@pytest.fixture
+def auto_login_user(db, client, create_user):
+    def make_auto_login():
+        user = create_user()
+        client.login(email=user.email, password=user.password)
+        return client, user
+    return make_auto_login
+
+
+@pytest.mark.django_db
+def test_logout_logged(auto_login_user):
+    client, _ = auto_login_user()
+    client.get('/logout/')
+    assertTemplateUsed(template_name='main.html')
+
+
+@pytest.mark.parametrize('url', ['/logout/', '/new_note/', '/receipt/'])
+@pytest.mark.django_db
+def test_log_required_views_unlogged(url, client):
+    client.get(url)
+    assertTemplateUsed(template_name='sign_in.html')
+
+
+@pytest.mark.django_db
+def test_log_in_incorrect_data(client):
+    response = client.post(
+        '/sign_in', {"email": "test@test.test", "password": "incorrect"})
+    assert response.url == '/sign_in/'
